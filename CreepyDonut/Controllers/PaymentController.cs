6f33904380a3370d50199afed0cb5b60e9361885
Albertus Tejasukmana <albertus.tejasukmana@binus.ac.id>
2025-05-23 15:54:33 +0700

    payment controller

---
+++ CreepyDonut/Controllers/PaymentController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreepyDonut.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        [HttpPost("get-snap-token")]
+        public async Task<IActionResult> GetSnapToken()
+        {
+            string serverKey = "SB-Mid-server-UKfeRACLdBo88p4AATKK7p6u"; // Ganti dengan server key kamu
+            string encodedAuth = Convert.ToBase64String(Encoding.UTF8.GetBytes(serverKey + ":"));
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", encodedAuth);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var json = JsonConvert.SerializeObject(new
+                {
+                    transaction_details = new
+                    {
+                        order_id = $"order-{Guid.NewGuid()}",
+                        gross_amount = 15000
+                    },
+                    customer_details = new
+                    {
+                        first_name = "NamaDepan",
+                        last_name = "NamaBelakang",
+                        email = "emailkamu@example.com",
+                        phone = "08123456789"
+                    },
+                    enabled_payments = new[] { "gopay", "credit_card" },
+                    credit_card = new
+                    {
+                        secure = true
+                    }
+                });
+
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync("https://app.sandbox.midtrans.com/snap/v1/transactions", content);
+                var result = await response.Content.ReadAsStringAsync();
+
+                return Content(result, "application/json");
+            }
+        }
+    }
+}
---
+++ CreepyDonut/Models/CheckoutRequest.cs
@@ -0,0 +1,31 @@
+﻿namespace CreepyDonut.Models
+{
+    public class CheckoutRequest
+    {
+        public string payment_type { get; set; }
+        public TransactionDetails transaction_details { get; set; }
+        public CreditCard credit_card { get; set; }
+        public CustomerDetails customer_details { get; set; }
+    }
+
+    public class TransactionDetails
+    {
+        public string order_id { get; set; }
+        public int gross_amount { get; set; }
+    }
+
+    public class CreditCard
+    {
+        public bool authentication { get; set; } // ganti dari "secure"
+        public string token_id { get; set; }     // token dari Snap atau dummy
+    }
+
+
+    public class CustomerDetails
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+    }
+}
